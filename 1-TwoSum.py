22f96d9833033bbc18ee93d446c507fc568b57ff
anthonydelphy <73381837+anthonydelphy@users.noreply.github.com>
2024-09-05 18:15:58 -0700

    Added file format extensions to all problems and added all two sum problems

---
+++ 1-TwoSum.py
@@ -0,0 +1,17 @@
+# Two-Loop solution. Time complexity of O(n).
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diction = {}
+        
+        # Create dictionary
+        # the key will be the value of the array while the actual value stored is the index where the value is found.
+        for i, x in enumerate(nums):
+            diction[x] = i
+
+        for i, x in enumerate(nums):
+        # Find the value that is required to get to the target and see if it exists in the dictionary. 
+            complement = target - nums[i]
+            #ensure that we are NOT looking at the same number we are currently iterating on. The answer must be different indexes
+            if complement in diction and diction[complement] != i:
+                return [i, diction[complement]]
---
+++ 167-TwoSumTwo.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(n)
+
+ # Since the given array is sorted already, we can simply use two pointers to solve the problem in O(n). The reason that we do not sort the problem in Two Sum I is because the time complexity of sorting in the best case scenario is O(n log n). The solution that we have for Two Sum I has a time complexity of O(n), so sorting it will slow it down.
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:        
+        # Create two left and right pointers
+        r = len(numbers) - 1
+        l = 0
+
+        #If the sum of the pointers is too small, move the left pointer rightward. If the sum of the pointers is too large, move the right pointer leftward. Since there is a guranteed answer, we will eventually reach the correct answer.
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
